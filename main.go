f18bafb9bd157675b242452af7ed0f8dad78831a
Aaron Fleming <amfleming15@gmail.com>
2026-01-31 01:40:24 -0800

    feat: add web server on port 8080

--- main.go
+++ main.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+	"time"
+)
 
 func main() {
+	const port = "8080"
 	fmt.Println("Hello, from chirpy!")
+	serveMux := http.NewServeMux()
+	server := &http.Server{
+		Addr:           ":" + port,
+		Handler:        serveMux,
+		ReadTimeout:    10 * time.Second, // from net/http example
+		WriteTimeout:   10 * time.Second, // from net/http example
+		MaxHeaderBytes: 1 << 20,          // from net/http example
+	}
+	fmt.Printf("Serving on port: %s\n", port)
+	server.ListenAndServe()
+
 }

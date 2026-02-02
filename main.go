517b6c4ffe18d1dea31d6175b6a5c75987d30206
Aaron Fleming <amfleming15@gmail.com>
2026-02-01 22:38:28 -0800

    feat: add fileserver metrics endpoints

--- main.go
+++ main.go
@@ -3,8 +3,41 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"sync/atomic"
 	"time"
 )
+
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
+func (cfg *apiConfig) middlewareHandlerMetrics(w http.ResponseWriter, req *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	msg := fmt.Sprintf("Hits: %d", cfg.fileserverHits.Load())
+	_, err := w.Write([]byte(msg))
+	if err != nil {
+		fmt.Printf("error writing response: %v\n", err)
+	}
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, req)
+	})
+}
+
+func (cfg *apiConfig) middlewareHandlerReset(w http.ResponseWriter, req *http.Request) {
+	cfg.fileserverHits.Store(0)
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	msg := "metrics reset"
+	_, err := w.Write([]byte(msg))
+	if err != nil {
+		fmt.Printf("error writing response: %v\n", err)
+	}
+}
 
 func handlerHealth(w http.ResponseWriter, req *http.Request) {
 	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
@@ -18,10 +51,18 @@ func handlerHealth(w http.ResponseWriter, req *http.Request) {
 
 func main() {
 	const port = "8080"
+	cfg := apiConfig{}
+
 	fmt.Println("Hello, from chirpy!")
+
 	serveMux := http.NewServeMux()
-	serveMux.Handle("/app/", http.StripPrefix("/app/", http.FileServer(http.Dir("./"))))
+
+	fileHandler := http.StripPrefix("/app/", http.FileServer(http.Dir("./")))
+	serveMux.Handle("/app/", cfg.middlewareMetricsInc(fileHandler))
+
 	serveMux.HandleFunc("/healthz", handlerHealth)
+	serveMux.HandleFunc("/metrics", cfg.middlewareHandlerMetrics)
+	serveMux.HandleFunc("/reset", cfg.middlewareHandlerReset)
 
 	server := &http.Server{
 		Addr:           ":" + port,
@@ -31,6 +72,9 @@ func main() {
 		MaxHeaderBytes: 1 << 20,          // from net/http example
 	}
 	fmt.Printf("Serving on port: %s\n", port)
-	server.ListenAndServe()
+	err := server.ListenAndServe()
+	if err != nil {
+		fmt.Printf("error encountered: %v\n", err)
+	}
 
 }

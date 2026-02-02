6ee65e21e5f75642d9d25d3b780c579f66704a34
Aaron Fleming <amfleming15@gmail.com>
2026-02-01 19:22:55 -0800

    feat: add readiness endpoint at /healthz and update fileserver endpoint to /app/

--- main.go
+++ main.go
@@ -6,11 +6,22 @@ import (
 	"time"
 )
 
+func handlerHealth(w http.ResponseWriter, req *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	msg := "OK"
+	_, err := w.Write([]byte(msg))
+	if err != nil {
+		fmt.Printf("error writing response: %v\n", err)
+	}
+}
+
 func main() {
 	const port = "8080"
 	fmt.Println("Hello, from chirpy!")
 	serveMux := http.NewServeMux()
-	serveMux.Handle("/", http.FileServer(http.Dir(".")))
+	serveMux.Handle("/app/", http.StripPrefix("/app/", http.FileServer(http.Dir("./"))))
+	serveMux.HandleFunc("/healthz", handlerHealth)
 
 	server := &http.Server{
 		Addr:           ":" + port,

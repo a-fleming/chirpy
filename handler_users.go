af52c6a9d2a326822e6add54fb36d5f75ee219a4
Aaron Fleming <amfleming15@gmail.com>
2026-02-02 23:25:46 -0800

    refactor: separate User struct for access by other handlers

--- handler_users.go
+++ handler_users.go
@@ -1,52 +1,27 @@
 package main
 
 import (
-	"chirpy/internal/auth"
 	"chirpy/internal/database"
-	"encoding/json"
 	"net/http"
 	"strings"
-	"time"
-
-	"github.com/google/uuid"
 )
 
 func (cfg *apiConfig) handlerUsers(w http.ResponseWriter, req *http.Request) {
-	type parameters struct {
-		Email    string `json:"email"`
-		Password string `json:"password"`
-	}
-	type User struct {
-		ID             uuid.UUID `json:"id"`
-		CreatedAt      time.Time `json:"created_at"`
-		UpdatedAt      time.Time `json:"updated_at"`
-		Email          string    `json:"email"`
-		HashedPassword string    `json:"hashed_password,omitempty"`
-	}
-
-	params := parameters{}
-
-	decoder := json.NewDecoder(req.Body)
-	err := decoder.Decode(&params)
+	email, hashedPassword, err := DecodeEmailAndHashedPassword(req)
 	if err != nil {
 		msg := "Unable to decode parameters"
 		respondWithError(w, http.StatusInternalServerError, msg, err)
 		return
 	}
 
-	if !isValidEmail(params.Email) {
+	if !isValidEmail(email) {
 		msg := "Invalid email address"
 		respondWithError(w, http.StatusBadRequest, msg, err)
 		return
 	}
-	hashedPassword, err := auth.HashPassword(params.Password)
-	if err != nil {
-		msg := "Unable to hash password"
-		respondWithError(w, http.StatusInternalServerError, msg, err)
-	}
 
 	paramsWithHashedPassword := database.CreateUserParams{
-		Email:          params.Email,
+		Email:          email,
 		HashedPassword: hashedPassword,
 	}
 
@@ -55,13 +30,7 @@ func (cfg *apiConfig) handlerUsers(w http.ResponseWriter, req *http.Request) {
 		respondWithError(w, http.StatusInternalServerError, "Unable to create user", err)
 		return
 	}
-	userWithoutHashedPassword := User{
-		ID:        user.ID,
-		CreatedAt: user.CreatedAt,
-		UpdatedAt: user.UpdatedAt,
-		Email:     user.Email,
-	}
-	respondWithJSON(w, http.StatusCreated, userWithoutHashedPassword)
+	respondWithJSON(w, http.StatusCreated, SanitizeUser(user))
 }
 
 func isValidEmail(email string) bool {
---
+++ user.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"chirpy/internal/auth"
+	"chirpy/internal/database"
+	"encoding/json"
+	"net/http"
+	"time"
+
+	"github.com/google/uuid"
+)
+
+type User struct {
+	ID             uuid.UUID `json:"id"`
+	CreatedAt      time.Time `json:"created_at"`
+	UpdatedAt      time.Time `json:"updated_at"`
+	Email          string    `json:"email"`
+	HashedPassword string    `json:"hashed_password,omitempty"`
+}
+
+func DecodeEmailAndPassword(req *http.Request) (string, string, error) {
+	type parameters struct {
+		Email    string `json:"email"`
+		Password string `json:"password"`
+	}
+
+	params := parameters{}
+
+	decoder := json.NewDecoder(req.Body)
+	err := decoder.Decode(&params)
+	if err != nil {
+		return "", "", err
+	}
+	return params.Email, params.Password, nil
+}
+
+func DecodeEmailAndHashedPassword(req *http.Request) (string, string, error) {
+	email, password, err := DecodeEmailAndPassword(req)
+	hashedPassword, err := auth.HashPassword(password)
+	if err != nil {
+		return "", "", err
+	}
+	return email, hashedPassword, nil
+}
+
+func SanitizeUser(user any) User {
+	switch u := user.(type) {
+	case User:
+		return User{
+			ID:        u.ID,
+			CreatedAt: u.CreatedAt,
+			UpdatedAt: u.UpdatedAt,
+			Email:     u.Email,
+		}
+	case database.User:
+		return User{
+			ID:        u.ID,
+			CreatedAt: u.CreatedAt,
+			UpdatedAt: u.UpdatedAt,
+			Email:     u.Email,
+		}
+	default:
+		return User{}
+	}
+}

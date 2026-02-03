b0da624983528245b3e7cdb47523120e38532fee
Aaron Fleming <amfleming15@gmail.com>
2026-02-02 19:21:34 -0800

    feat: add users table reset from /admin/reset

--- handler_reset.go
+++ handler_reset.go
@@ -10,10 +10,17 @@ func (cfg *apiConfig) handlerReset(w http.ResponseWriter, req *http.Request) {
 		respondWithError(w, http.StatusForbidden, msg, nil)
 		return
 	}
+	err := cfg.db.Reset(req.Context())
+	if err != nil {
+		msg := "Unable to reset users"
+		respondWithError(w, http.StatusInternalServerError, msg, err)
+		return
+	}
 
 	cfg.fileserverHits.Store(0)
 	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 	w.WriteHeader(http.StatusOK)
 	msg := "Hits reset to 0"
 	w.Write([]byte(msg))
+
 }

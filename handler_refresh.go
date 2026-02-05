afa0dcd6babe28abdd4eab56771f405a366e818b
Aaron Fleming <amfleming15@gmail.com>
2026-02-04 19:42:59 -0800

    fix: check for expired token before refreshing

--- handler_refresh.go
+++ handler_refresh.go
@@ -4,12 +4,14 @@ import (
 	"chirpy/internal/auth"
 	"database/sql"
 	"net/http"
+	"time"
 )
 
 func (cfg *apiConfig) handlerRefresh(w http.ResponseWriter, req *http.Request) {
 	type refreshResponse struct {
 		JWTToken string `json:"token"`
 	}
+	now := time.Now().UTC()
 
 	refreshToken, err := auth.GetBearerToken(req.Header)
 	if err != nil {
@@ -28,6 +30,10 @@ func (cfg *apiConfig) handlerRefresh(w http.ResponseWriter, req *http.Request) {
 		}
 		return
 	} else if refreshTokenDetails.RevokedAt.Valid {
+		msg := "401 Unauthorized"
+		respondWithError(w, http.StatusUnauthorized, msg, err)
+		return
+	} else if refreshTokenDetails.ExpiresAt.Compare(now) <= 0 {
 		msg := "401 Unauthorized"
 		respondWithError(w, http.StatusUnauthorized, msg, err)
 		return
